0a8f089daa66dffc437362c1379d8bd0bdf33e22
HansNorg <52379922+HansNorg@users.noreply.github.com>
2023-11-01 12:30:58 +0100

    Update __init__.py

--- quantumNEAT/__init__.py
+++ quantumNEAT/__init__.py
@@ -1,4 +1,9 @@
 import logging
 from quantumneat.logger import setup_logger
 
-setup_logger(test_level=logging.INFO, file_level=logging.INFO)
+debugging = False
+
+if debugging:
+    setup_logger(test_level=logging.DEBUG, file_level=logging.DEBUG)
+else:
+    setup_logger(test_level=logging.INFO, file_level=logging.INFO)
