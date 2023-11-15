625dd1c50a823654dc389fcf64fc965908db6ea2
HansNorg <52379922+HansNorg@users.noreply.github.com>
2023-11-15 15:26:37 +0100

    Log errors to logger instead of std.out

--- quantumNEAT/quantumneat/logger.py
+++ quantumNEAT/quantumneat/logger.py
@@ -1,6 +1,9 @@
+import sys
 import logging
-    
-def setup_logger(name = "quantumNEAT", console_level = logging.WARNING, main_file_level = logging.INFO, quantumneat_level = logging.INFO, test_level = logging.DEBUG, experiments_level = logging.INFO, mode ="a", print_start = True, extra_file_name = ""):
+import traceback
+
+def setup_logger(name = "quantumNEAT", console_level = logging.WARNING, main_file_level = logging.INFO, quantumneat_level = logging.INFO, 
+                 test_level = logging.DEBUG, experiments_level = logging.INFO, mode ="a", print_start = True, extra_file_name = "", log_errors = True):
     # print(extra_file_name+"setup_logger")
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(module)s - %(filename)s - %(funcName)s - line: %(lineno)d\n%(message)s')
 
@@ -55,9 +58,20 @@ def setup_logger(name = "quantumNEAT", console_level = logging.WARNING, main_fil
 
     if print_start:
         logger.info("==================logger_setup==================")
-        quantumneat_logger.info("======logger_setup==================")
-        test_logger.info("============logger_setup==================")
-        experiments_logger.info("======logger_setup==================")
+        quantumneat_logger.info("==================logger_setup==================")
+        test_logger.info("==================logger_setup==================")
+        experiments_logger.info("==================logger_setup==================")
+    
+    # log uncaught exceptions
+    def handle_exception(exc_type, exc_value, exc_traceback):
+        if issubclass(exc_type, KeyboardInterrupt):
+            logger.error("================= Keyboard interruption =================")
+            sys.__excepthook__(exc_type, exc_value, exc_traceback) # calls default excepthook
+            return
+        logger.error("================= Uncaught exception =================", exc_info=(exc_type, exc_value, exc_traceback))
+        
+    if log_errors:
+        sys.excepthook = handle_exception
 
 def default_logger(debugging = False, print_start = True, extra_file_name=""):
     if debugging:
