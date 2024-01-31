21718378314214b907bdeebe84b73ae92fba1ff2
HansNorg <52379922+HansNorg@users.noreply.github.com>
2024-01-31 16:19:13 +0100

    Update constant_population.sh

--- quantumNEAT/experiments/constant_population.sh
+++ quantumNEAT/experiments/constant_population.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 bash experiments/run_experiment.bash tfim linear_growth -O 100 -R 10
-bash experiments/run_experiment.bash tfim linear_growth -O 100 -R 10 -X "no-force"
+bash experiments/run_experiment.bash tfim linear_growth -O 100 -R 1 -X "no-force"
+bash experiments/run_experiment.bash tfim linear_growth -O 100 -R 1 -X "no-force"
+bash experiments/run_experiment.bash tfim linear_growth -O 100 -R 1 -X "no-force"
+bash experiments/run_experiment.bash tfim linear_growth -O 100 -R 1 -X "no-force"
+bash experiments/run_experiment.bash tfim linear_growth -O 100 -R 1 -X "no-force"
+bash experiments/run_experiment.bash tfim linear_growth -O 100 -R 1 -X "no-force"
+bash experiments/run_experiment.bash tfim linear_growth -O 100 -R 1 -X "no-force"
+bash experiments/run_experiment.bash tfim linear_growth -O 100 -R 1 -X "no-force"
+bash experiments/run_experiment.bash tfim linear_growth -O 100 -R 1 -X "no-force"
+bash experiments/run_experiment.bash tfim linear_growth -O 100 -R 1 -X "no-force"
 bash experiments/run_experiment.bash tfim linear_growth -O 100 -R 10 -X "normalise"
 bash experiments/run_experiment.bash tfim linear_growth -O 100 -R 10 -X "no-force_normalise"
