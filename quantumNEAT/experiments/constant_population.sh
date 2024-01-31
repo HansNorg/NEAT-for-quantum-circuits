68d2b02a30eac4be1e35c36e9a44c47b73cee677
HansNorg <52379922+HansNorg@users.noreply.github.com>
2024-01-31 16:11:56 +0100

    Create constant_population.sh
    New test for cluster

---
+++ quantumNEAT/experiments/constant_population.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+bash experiments/run_experiment.bash tfim linear_growth -O 100 -R 10
+bash experiments/run_experiment.bash tfim linear_growth -O 100 -R 10 -X "no-force"
+bash experiments/run_experiment.bash tfim linear_growth -O 100 -R 10 -X "normalise"
+bash experiments/run_experiment.bash tfim linear_growth -O 100 -R 10 -X "no-force_normalise"
