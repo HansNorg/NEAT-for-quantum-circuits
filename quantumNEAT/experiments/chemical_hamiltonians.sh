1f469e281380abfb92401f2deaaf1437bde2ced8
HansNorg <52379922+HansNorg@users.noreply.github.com>
2024-02-13 09:52:58 +0100

    Update chemical_hamiltonians.sh

--- quantumNEAT/experiments/chemical_hamiltonians.sh
+++ quantumNEAT/experiments/chemical_hamiltonians.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-bash run_experiment_lorentz.bash h6_all_errorless -N 6 -O 200 -R 5
-bash run_experiment_lorentz.bash lih_all_errorless -N 10 -O 200 -R 5
+bash experiments/run_experiment_lorentz.bash h6_all_errorless -N 6 -O 200 -R 5
+bash experiments/run_experiment_lorentz.bash lih_all_errorless -N 10 -O 200 -R 5
