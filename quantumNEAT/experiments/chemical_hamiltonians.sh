1de0509bf6ed1815f2f5e6684c9c0a3be47fcc0f
HansNorg <52379922+HansNorg@users.noreply.github.com>
2024-02-13 09:54:54 +0100

    Update chemical_hamiltonians.sh

--- quantumNEAT/experiments/chemical_hamiltonians.sh
+++ quantumNEAT/experiments/chemical_hamiltonians.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-bash experiments/run_experiment_lorentz.bash h6_all_errorless_no-solution -N 6 -O 200 -R 5
-bash experiments/run_experiment_lorentz.bash lih_all_errorless_no-solution -N 10 -O 200 -R 5
+bash experiments/run_experiment_lorentz.bash h6_all_errorless_no-solution linear_growth -N 6 -O 200 -R 5
+bash experiments/run_experiment_lorentz.bash lih_all_errorless_no-solution linear_growth -N 10 -O 200 -R 5
