a4134e5027f461b1a82a7665e4554354be4ced02
HansNorg <52379922+HansNorg@users.noreply.github.com>
2024-02-24 14:12:02 +0100

    Big experiment

--- quantumNEAT/experiments/chemical_hamiltonians.sh
+++ quantumNEAT/experiments/chemical_hamiltonians.sh
@@ -2,6 +2,29 @@
 
 # bash experiments/run_experiment_lorentz.bash h6_all_errorless_no-solution linear_growth -N 6 -O 200 -R 5
 # bash experiments/run_experiment_lorentz.bash lih_all_errorless_no-solution linear_growth -N 10 -O 200 -R 5
-bash experiments/run_experiment_variable.bash gs_h2_errorless linear_growth -N 2 -O 200 -R 5 -G 20
-bash experiments/run_experiment_variable.bash gs_h6_errorless linear_growth -N 6 -O 200 -R 5 -G 20
-bash experiments/run_experiment_variable.bash gs_lih_errorless linear_growth -N 10 -O 200 -G 20
+# bash experiments/run_experiment_variable.bash gs_h2_errorless linear_growth -N 2 -O 200 -R 5 -G 20
+# bash experiments/run_experiment_variable.bash gs_h6_errorless linear_growth -N 6 -O 200 -R 5 -G 20
+# bash experiments/run_experiment_variable.bash gs_lih_errorless linear_growth -N 10 -O 200 -G 20
+bash experiments/run_experiment_lorentz_single_cpu.bash gs_h2_errorless_saveh linear_growth -N 2
+bash experiments/run_experiment_lorentz_single_cpu.bash gs_h6_errorless_saveh linear_growth -N 6
+bash experiments/run_experiment_lorentz_single_cpu.bash gs_lih_errorless_saveh linear_growth -N 8
+
+bash experiments/run_experiment_lorentz_single_cpu.bash gs_h2_errorless_saveh linear_growth -N 2 -O 100
+bash experiments/run_experiment_lorentz_single_cpu.bash gs_h6_errorless_saveh linear_growth -N 6 -O 100
+bash experiments/run_experiment_lorentz_single_cpu.bash gs_lih_errorless_saveh linear_growth -N 8 -O 100
+
+bash experiments/run_experiment_lorentz_single_cpu.bash gs_h2_errorless_saveh linear_growth -N 2 -O 200
+bash experiments/run_experiment_lorentz_single_cpu.bash gs_h6_errorless_saveh linear_growth -N 6 -O 200
+bash experiments/run_experiment_lorentz_single_cpu.bash gs_lih_errorless_saveh linear_growth -N 8 -O 200
+
+bash experiments/run_experiment_lorentz_single_cpu.bash gs_h2_errorless_saveh qneat -N 2
+bash experiments/run_experiment_lorentz_single_cpu.bash gs_h6_errorless_saveh qneat -N 6
+bash experiments/run_experiment_lorentz_single_cpu.bash gs_lih_errorless_saveh qneat -N 8
+
+bash experiments/run_experiment_lorentz_single_cpu.bash gs_h2_errorless_saveh qneat -N 2 -O 100
+bash experiments/run_experiment_lorentz_single_cpu.bash gs_h6_errorless_saveh qneat -N 6 -O 100
+bash experiments/run_experiment_lorentz_single_cpu.bash gs_lih_errorless_saveh qneat -N 8 -O 100
+
+bash experiments/run_experiment_lorentz_single_cpu.bash gs_h2_errorless_saveh qneat -N 2 -O 200
+bash experiments/run_experiment_lorentz_single_cpu.bash gs_h6_errorless_saveh qneat -N 6 -O 200
+bash experiments/run_experiment_lorentz_single_cpu.bash gs_lih_errorless_saveh qneat -N 8 -O 200
