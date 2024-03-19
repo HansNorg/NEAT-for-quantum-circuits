919f1674748f418df9d6fe522cfbc55b5dfac4ff
HansNorg <52379922+HansNorg@users.noreply.github.com>
2024-03-19 14:10:09 +0100

    New experiments

---
+++ quantumNEAT/experiments/run_experiment_single_cpu_long.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+read -p "Which partition? " partition
+sbatch <<EOT
+#!/bin/bash
+#SBATCH --job-name=$1_$2
+#SBATCH --output=slurm_out/%x_%j.out
+#SBATCH --mail-user="hans.norg99@gmail.com"
+#SBATCH --mail-type="ALL"
+
+#SBATCH --partition=$partition
+#SBATCH --time=4-00:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=1G
+
+module load ALICE/default
+module load Python/3.10.8-GCCcore-12.2.0
+source "/home/s3727599/.cache/pypoetry/virtualenvs/quantumneat-gXCYO08V-py3.10/bin/activate"
+echo "[\$SHELL] #### Starting Python test at \$(date)"
+echo $@
+python ./experiments/run_experiment.py $@
+echo "[\$SHELL] #### Finished Test at \$(date)."
+EOT
