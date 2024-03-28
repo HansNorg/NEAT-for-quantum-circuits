7f0607b490342f5a9ce8d92d4ec2cf28a4d118c6
HansNorg <52379922+HansNorg@users.noreply.github.com>
2024-03-28 14:32:33 +0100

    HF

--- quantumNEAT/experiments/run_batch12-13-14.bash
+++ quantumNEAT/experiments/run_batch12-13-14.bash
@@ -1,18 +1,20 @@
 #!/bin/bash
-#SBATCH --job-name=h6
-#SBATCH --output=slurm_out/%x_%A.out
+#SBATCH --job-name=HF
+#SBATCH --output=slurm_out/%x_%A_%a.out
 #SBATCH --mail-user="hans.norg99@gmail.com"
 #SBATCH --mail-type="ALL"
 
 #SBATCH --partition=cpu-medium
-#SBATCH --time=1-00:00:00
+#SBATCH --time=10:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=1G
+
+#SBATCH --array=12,13,14
 
 module load ALICE/default
 module load Python/3.10.8-GCCcore-12.2.0
 source "/home/s3727599/.cache/pypoetry/virtualenvs/quantumneat-gXCYO08V-py3.10/bin/activate"
 echo "[$SHELL] #### Starting Python test at $(date)"
-python ./experiments/run_batch.py 13 0
+python ./experiments/run_batch.py $SLURM_ARRAY_TASK_ID 0
 echo "[$SHELL] #### Finished Test at $(date)."
